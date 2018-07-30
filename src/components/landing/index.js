f3bff45f27c37787cf59f4f6052d3efe2e4bd819
Sarah Geyer <sjgeyer@comcast.net>
2018-07-30 15:24:06 -0700

    add styling to landing page, add color scheme to vars.scss

--- src/components/landing/index.js
+++ src/components/landing/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './landing.scss';
 
 export default class Landing extends React.Component {
   render() {
     return (
       <div className='landing-page fade-in'>
+        <img src='http://via.placeholder.com/960x300'/>
         <p className='hero-text'>
           Hello Team!
         </p>
--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import autoBind from '../../utils/auto-bind';
 
 export default class Login extends React.Component {
@@ -7,6 +8,7 @@ export default class Login extends React.Component {
     this.state = {
       username: '',
       password: '',
+      secret: '',
     };
     autoBind.call(this, Login);
   }
@@ -17,28 +19,46 @@ export default class Login extends React.Component {
   }
 
   handleSubmit(e) {
-    // do something
+    // verify with backend
   }
 
   render() {
+    const buttonText = this.props.type === 'signup' ? 'Sign Up' : 'Log In';
     return (
-      <form onSubmit={this.handleSubmit} className='fade-in'>
-        <input
-          type='text'
-          name='username'
-          placeholder='username'
-          value={this.state.username}
-          onChange={this.handleChange}
-        />
-        <input
-          type='password'
-          name='password'
-          placeholder='password'
-          value={this.state.password}
-          onChange={this.handleChange}
-        />
-        <button type='submit'>Log In</button>
-      </form>
+      <div>
+        <form onSubmit={this.handleSubmit} className='fade-in'>
+          <input
+            type='text'
+            name='username'
+            placeholder='username'
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
+          <input
+            type='password'
+            name='password'
+            placeholder='password'
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+
+          {
+            this.props.type === 'signup'
+            && <input
+              type='text'
+              name='secret'
+              placeholder='secret'
+              value={this.state.secret}
+              onChange={this.handleChange}
+            />
+          }
+          <button type='submit'>{buttonText}</button>
+        </form>
+      </div>
     );
   }
 }
+
+Login.propTypes = {
+  type: PropTypes.string,
+};
